b3f5e3416422ee2ffecb76b087bdc8718bc2414d
satya4498 <ss7859868@gmail.com>
2020-12-13 21:03:36 +0530

    UC1 checking for employee attendance

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,2 +1,15 @@
+#!/bin/bash
 echo "Welcome to employee wage computation program"
-echo"Absent Present"
+ #UC1
+echo "Checking whether employee is present or absent"
+function employeeAttendance()
+{
+    Attendance=$((RANDOM%2))
+    if [ $Attendance -eq 1 ]
+    then
+         echo "employee is present"
+    else
+         echo "employee is absent"
+     fi
+}
+employeeAttendance
